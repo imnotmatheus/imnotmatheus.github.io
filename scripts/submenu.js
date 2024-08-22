4ab077d204d9a67a1a764da4962df8850c6e7e95
Matheus Fellipe Reguta <matheus.f.reguta@gmail.com>
2024-08-22 20:29:13 -0300

    ajuste para diferentes telas

--- scripts/submenu.js
+++ scripts/submenu.js
@@ -1,7 +1,22 @@
+// ajuste para mobile
+function moveSubmenus() {
+    const submenus = document.querySelectorAll('.submenu');
+    // Se for desktop ou tablet (tela > 768px), coloca os submenus fora do menu
+    if (window.innerWidth > 768) {
+        submenus.forEach(submenu => {
+            document.body.appendChild(submenu);
+        });
+    }
+}
+
+window.addEventListener('load', moveSubmenus);
+
 // seleciona todos os elementos menu-item, faz uma lista e itera
 document.querySelectorAll('.menu-item').forEach(item => {
     item.addEventListener('click', () => {
+        const menuId = item.getAttribute('id');
         const targetId = item.getAttribute('data-target');
+        const menu = document.getElementById(menuId);
         const submenu = document.getElementById(targetId);
         const isVisible = submenu.style.display == 'block';
 
